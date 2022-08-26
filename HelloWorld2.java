f86cb761ccb976967f43a96954c84af53a350cc5
23nhanna <112183704+23nhanna@users.noreply.github.com>
2022-08-26 09:58:01 -0400

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,30 @@
+//Nadia Hanna
+//8-25-22
+//Hello World Mug
+//Outputs a text-art Hello World Mug to the console
+
+public class HelloWorld2 {
+	
+	public static void main(String[] args) {
+		
+		//Make a multi-line string
+		String mug = "             d\n" +
+				"      o     l\n" +
+				"     l     r\n" +
+				"      l     o\n" +
+				"     e     w\n" +
+				"    h\n" +
+				"XXXXXXXXXXXXXXXXXXXX\n" +
+				"X               X   X\n" +
+				"X     JAVA      X   X\n" +
+				"X               XXXX\n" +
+				"X    IS HOT     X\n" +
+				"X               X\n" +
+				"X               X\n" +
+				"XXXXXXXXXXXXXXXXX\n";
+		
+		System.out.println(mug); //Output the multi-line string
+		
+	}
+
+}
